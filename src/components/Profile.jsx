a18bba983b547519079d923958cfaab18cc62b87
AlgorStudy <info@algor.kz>
2023-05-04 17:32:58 +0600

    change profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
       <img src='https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w=' />
     </div>
     <div>
-      ava + description
+      moldir
     </div>
     <div>
       My posts
